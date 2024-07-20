63844fd6bc5c55f7c40ef30f77df3d4fa973da05
Nebojsa Knezevic <global021man@gmail.com>
2024-07-20 14:10:52 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+const babelPlugins = [];
+
+if (process.env.MIGHTYMELD) {
+  babelPlugins.push('mightymeld/babel-plugin-mightymeld');
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react({
+      babel: {
+        plugins: babelPlugins
+      }
+    })
+  ]
+});
